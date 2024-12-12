c5c43aebd36bb63b72b80127d92b13728c91903d
carleyandelin <carleyna@byu.edu>
2024-12-11 17:57:45 -0700

    made a quote italisized

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -21,7 +21,7 @@ export function Login({ userName, authState, onAuthChange }) {
     <main className={mainStyle}>
       <h1>Welcome to BowlerBlitz!</h1>
       <div>
-        {authState !== AuthState.Unknown && <h3>Life’s a game, bowling’s serious.</h3>}
+        {authState !== AuthState.Unknown && <h4><em>Life’s a game, bowling’s serious.</em></h4>}
         {authState === AuthState.Authenticated && (
           <Authenticated userName={userName} onLogout={() => onAuthChange(userName, AuthState.Unauthenticated)} />
         )}
